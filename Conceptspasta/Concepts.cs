7957e322a2fcf32c8ae61c320396f4e0a373651b
Elisa de Oliveira Martins <elisa98martins@gmail.com>
2021-05-17 19:06:38 -0300

    parte do modulo

---
+++ Conceptspasta/Concepts.cs
@@ -0,0 +1,86 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text.RegularExpressions;
+
+// namespace concepts
+// {
+//     // o que são classes?
+//     // planta pra criar um objeto
+//     //elas permitem que o usuario crie seu proprio tipo de objetos
+//     public class Monster{
+//         //  o que uma classe pode ter
+//         // membro de dados - constantes/variaveis
+//         // metodos/funções
+
+//         // public - pode ser acessado em qualquer lugar no projeto
+//         // public dados de membros/metodos - pode ser acessado qualquer lugar da classe
+//         // was instantiated
+//         public string name_;
+//         public int size_;
+//         public const int legs_ = 2;
+
+//         // private dados de membros/metodos - pode APENAS ser acessado da classe
+//         private int scare_;
+
+//         // static dados de membros/metodos - aplicado para a classe inteira do q a instancia nela
+//         public static int nMonsters_;
+
+//         // default constructor -initiated all values
+//         public Monster(){
+//             name_ = "default";
+//             size_ = 20;
+//             scare_ = 10;
+//             nMonsters_++; // aumenta numero de monstros
+//         }
+
+//         // constructors
+//         public Monster(string name, int size, int scare){
+//             name_ = name;
+//             size_ = size;
+//             scare_ = scare;
+//             nMonsters_++;
+//         }
+
+//         // methods
+//         public void print(){
+//             Console.WriteLine("Monsters name: " + name_);
+//             Console.WriteLine("Monsters size: " + size_);
+//             Console.WriteLine("Mosnters scare: " + scare_);
+//         }
+//     }
+//     public class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Monster Dinosour = new Monster();
+//             // public members
+//             Console.WriteLine(Dinosour.name_);
+//             // can be changed
+//             Dinosour.name_ = "Rex";
+//             Console.WriteLine(Dinosour.name_);
+
+//             // change a const cannot be changed
+//             // example Dinosour.legs_ = 3;
+
+//             // private members cannot be accessed here
+//             // example -- Console.WriteLine(Dinosour.scare_);
+
+//             // using non-default-constructor
+//             Console.WriteLine(Monster.nMonsters_);
+//             Monster Dragon = new Monster("Drako", 30, 25);
+//             Console.WriteLine(Dragon.name_);
+
+//             // static check
+//             Console.WriteLine(Monster.nMonsters_);
+
+//             Monster Zombie = new Monster("Zombie", 6, 8);
+//             // call print
+//             Zombie.print();
+//             Dragon.print();
+//             Dinosour.print();
+
+//             Console.WriteLine(Monster.nMonsters_);
+//         }
+//     }
+// }
---
+++ Conceptspasta/Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Conceptspasta{
+
+
+
+// inheritance
+// class inherits methods and data members from the class above
+
+// abstract class
+// cannot instantiated
+// can contain abstract methods but its not required
+// can contain non abstract methods
+
+    public abstract class Character{
+        public string name_;
+        public int speed_;
+        public int health_;
+        public int test_ = 2;
+
+        public abstract void print(); // MUST BE modified in inheriting classes
+
+        public testFunction(){
+            test_ = test_*2;
+            return test_;
+        }
+
+        // virtual class
+        // can be changed in classes that derive from the base class BUT NOT required
+        public virtual void Swing(){
+            Console.WriteLine("SWING!");
+        }
+    }
+
+    public class SwordsMan : Character{
+        public SwordsMan(){
+            name_ = "Erik";
+            speed_ = 10;
+            health_ = 100;
+        }
+
+        // implement abstract class we must use override
+        public override void print()
+        {
+            Console.WriteLine("My name is "+ name_);
+            Console.WriteLine("and I am " + speed_ + " fast");
+            Console.WriteLine("my health is " + health_);
+        }
+    }
+
+        public class Program
+        {
+            public static void Main(String[] args){
+                SwordsMan Erik = new SwordsMan();
+                Erik.print();
+                Erik.testFunction();
+                Console.WriteLine(Erik.test_);
+            }
+        }
+}
